e9b0263e9dfdc21c1410e8d5029541da8a03f48e
miqur <treshom@gmail.com>
2019-06-26 16:59:09 +0300

    Revert "drag-and-drop-864"
    This reverts commit baccc48a

--- js/interface.js
+++ js/interface.js
@@ -19,7 +19,6 @@ var $pagination = $('.pagination');
 var $goToFolderAlertTimeout = 5000;
 var $spinner = $('.spinner-holder');
 var $newBtn = $('.new-btn');
-var $goToFolderAlertTimeout = 5000;
 
 // This should contain either app/org/folder of current folder
 var currentSelection;
