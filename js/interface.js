c9008c386a3eafe234fcb6cbbea13f5a3d4324b0
AndrRyaz <71251809+AndrRyaz@users.noreply.github.com>
2021-04-14 12:23:52 +0300

    smal refactor

--- js/interface.js
+++ js/interface.js
@@ -429,6 +429,10 @@ function deletionModalConfirm(element) {
               className: 'btn-primary'
             },
           },
+        }).then(function() {
+          if (!result) {
+            $('[data-browse-trash]').click();
+          }
         });
       });
 
@@ -559,10 +563,6 @@ function restoreTrashItems(items) {
       Fliplet.Modal.alert({
         title: 'Restore failed',
         message: Fliplet.parseError(error),
-      }).then(function() {
-        if (!result) {
-          $('[data-browse-trash]').click();
-        }
       });
     });
   });
