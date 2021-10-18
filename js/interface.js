421c09cac982e0991e24f8398fcfeabef527bf22
Hugo Carneiro <hello@thegeekdeveloper.com>
2021-10-18 18:47:56 +0100

    Moved to fliplet-studio-ui

--- js/interface.js
+++ js/interface.js
@@ -1641,20 +1641,6 @@ function generateDeleteMessage(items) {
   return 'Are you sure you want to delete the selected items?\nAll the content inside any folders will be deleted too.';
 }
 
-function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) {
-    return '0 Bytes';
-  }
-
-  var k = 1024;
-  var dm = decimals < 0 ? 0 : decimals;
-  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-  var i = Math.floor(Math.log(bytes) / Math.log(k));
-
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-}
-
 function toggleStorageUsage($el) {
   var selectedAppId = $el.data('app-id');
 
